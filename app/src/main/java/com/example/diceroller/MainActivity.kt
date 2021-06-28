e7512dc806fe3643bffb4065261a8fb72a42fe0a
N+x <noxembourg@gmail.com>
2021-06-29 07:27:01 +0800

    1.3 coding challenge

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,12 +10,14 @@ import android.widget.ImageView
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         val btnRoll: Button = findViewById(R.id.roll_button)
 
@@ -23,17 +25,25 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
 
-        val btnCount: Button = findViewById(R.id.countup_button)
-        btnCount.setOnClickListener (){
-            countUp()
-        }
+        // 1.2 coding challenge
+//        val btnCount: Button = findViewById(R.id.countup_button)
+//        btnCount.setOnClickListener (){
+//            countUp()
+//        }
     }
 
 
     private fun rollDice() {
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+        // Toast.makeText(this,"Button is click",Toast.LENGTH_LONG).show()
+        // A toast is a view that shows the user a simple message in a small popup window.
+    }
+
+    private fun getRandomDiceImage() : Int {
         val randomInt = (1..6).random()
 
-        val drawableResource = when (randomInt) {
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -41,22 +51,20 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
-        // Toast.makeText(this,"Button is click",Toast.LENGTH_LONG).show()
-        // A toast is a view that shows the user a simple message in a small popup window.
     }
 
-    private fun countUp() {
-//        val tvResult: TextView = findViewById(R.id.result_text)
-//            // increment the number up to 6.
-//            // The text value in resultText.text is an instance of the CharSequence class;
-//            // it needs to be converted to a String object before it can be converted to an int.
-//            var resultInt = tvResult.text.toString().toInt()
-//
-//            if (resultInt < 6) {
-//                resultInt++
-//                tvResult.text = resultInt.toString()
-//            }
-        }
+    // 1.2 coding challenge
+//    private fun countUp() {
+////        val tvResult: TextView = findViewById(R.id.result_text)
+////            // increment the number up to 6.
+////            // The text value in resultText.text is an instance of the CharSequence class;
+////            // it needs to be converted to a String object before it can be converted to an int.
+////            var resultInt = tvResult.text.toString().toInt()
+////
+////            if (resultInt < 6) {
+////                resultInt++
+////                tvResult.text = resultInt.toString()
+////            }
+//        }
     }
 
